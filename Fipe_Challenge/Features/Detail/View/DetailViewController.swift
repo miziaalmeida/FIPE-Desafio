95244eb0b51bdd703dc99de7ee28e670a92f9834
Jeiel Lima <112661437+jeiellima@users.noreply.github.com>
2024-02-19 18:46:47 -0300

    add: adicionando arquivos
    DetailViewController e DetailViewModel

---
+++ Fipe_Challenge/Features/Detail/View/DetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.swift
+//  Fipe_Challenge
+//
+//  Created by Jeiel Lima on 19/02/24.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
---
+++ Fipe_Challenge/Features/Detail/ViewModel/DetailViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  DetailViewModel.swift
+//  Fipe_Challenge
+//
+//  Created by Jeiel Lima on 19/02/24.
+//
+
+import Foundation
