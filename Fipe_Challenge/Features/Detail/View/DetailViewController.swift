b970ecc74f9631e806a378bb6a351ed274cfd220
Jeiel Lima <112661437+jeiellima@users.noreply.github.com>
2024-02-20 17:17:31 -0300

    add: adicionando componentes

--- Fipe_Challenge/Features/Detail/View/DetailViewController.swift
+++ Fipe_Challenge/Features/Detail/View/DetailViewController.swift
@@ -9,9 +9,82 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
+    var details: [CarDetail]?
+    var brandId: String?
+    var modelId: String?
+    var yearId: String?
+    var viewModel = DetailViewModel()
+    
+    lazy var brandLabel: UILabel = {
+        let brandLabel = UILabel()
+        brandLabel.translatesAutoresizingMaskIntoConstraints = false
+        return brandLabel
+    }()
+    
+    lazy var modelLabel: UILabel = {
+        let modelLabel = UILabel()
+        modelLabel.translatesAutoresizingMaskIntoConstraints = false
+       return modelLabel
+    }()
+    
+    lazy var yearLabel: UILabel = {
+        let yearLabel = UILabel()
+        yearLabel.translatesAutoresizingMaskIntoConstraints = false
+        return yearLabel
+    }()
+    
+    lazy var priceLabel: UILabel = {
+        let priceLabel = UILabel()
+        priceLabel.translatesAutoresizingMaskIntoConstraints = false
+        return priceLabel
+    }()
+    
+    lazy var viewCentral: UIView = {
+        let view = UIView()
+        view.backgroundColor = .systemCyan
+        view.layer.cornerRadius = 10
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        viewModel.brandId = brandId
+        viewModel.modelId = modelId
+        viewModel.yearId = yearId
+        self.viewModel.getDetailForYears { details in
+            self.details = details
+            DispatchQueue.main.async {
+                self.setupLabels()
+            }
+        }
+    }
+    
+    func setupLabels() {
+        guard let details = details?.first else {
+            return
+        }
 
+        brandLabel.text = "Marca: \(details.marca)"
+        modelLabel.text = "Modelo: \(details.modelo)"
+        yearLabel.text = "Ano: \(details.anoModelo)"
+        priceLabel.text = "Valor: \(details.valor)"
+        
+        view.addSubview(viewCentral)
+        view.addSubview(brandLabel)
+        view.addSubview(modelLabel)
+        view.addSubview(yearLabel)
+        view.addSubview(priceLabel)
+        
+        setupConstraint()
+    }
+    
+    func setupConstraint() {
+        NSLayoutConstraint.activate([
+            viewCentral.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            viewCentral.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            
+        ])
     }
 
 }
